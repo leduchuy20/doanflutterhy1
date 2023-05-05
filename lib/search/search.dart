eae1359f1935ad829f5fa2bd7c34d92c353e2c53
leduchuy20 <ducuyyle@gmail.com>
2023-05-05 22:36:51 +0700

    5_5

--- lib/search/search.dart
+++ lib/search/search.dart
@@ -21,129 +21,126 @@ class _SearchState extends State<Search> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text(
-          "Search",
-          style: TextStyle(
-              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Text(
+            "Search",
+            style: TextStyle(
+                fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+          ),
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: FaIcon(FontAwesomeIcons.arrowLeft),
+            color: Colors.black,
+          ),
         ),
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: FaIcon(FontAwesomeIcons.arrowLeft),
-          color: Colors.black,
-        ),
-      ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: productspre.snapshots(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (snapshot.hasError) {
-            return Text('Something went wrong');
-          }
-
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          }
-          var products = snapshot.data!.docs
-              .map((doc) => Products.fromFirestore(doc))
-              .toList();
-          // final searchResults = products
-          //     .where((product) => product.title!
-          //         .toString()
-          //         .toLowerCase()
-          //         .contains(searchQuery.toLowerCase()))
-          //     .toList();
-
-          products = products
-              .where((product) => product.title!
-                  .toString()
-                  .toLowerCase()
-                  .contains(searchQuery.toLowerCase()))
-              .toList();
-
-          return Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Search For Spa",
-                  style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                TextField(
-                  onChanged: (value) {
-                    setState(() {
-                      searchQuery = value;
-                    });
-                  },
-                  decoration: InputDecoration(
-                    filled: true,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8),
-                        borderSide:
-                            BorderSide(color: Colors.pink.shade200, width: 2)),
-                    hintText: "Input Text",
-                    prefixIcon: Hero(
-                        tag: "search",
-                        child: Icon(
-                          Icons.search,
-                          color: Colors.black,
-                          size: 30,
-                        )),
+        body: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Search For Spa",
+                style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              TextField(
+                onChanged: (value) {
+                  setState(() {
+                    searchQuery = value;
+                  });
+                },
+                decoration: InputDecoration(
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
                   ),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide:
+                          BorderSide(color: Colors.pink.shade200, width: 2)),
+                  hintText: "Input Text",
+                  prefixIcon: Hero(
+                      tag: "search",
+                      child: Icon(
+                        Icons.search,
+                        color: Colors.black,
+                        size: 30,
+                      )),
                 ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                Divider(
-                  color: Colors.pink.shade200,
-                ),
-                ListView.builder(
-                  scrollDirection: Axis.vertical,
-                  shrinkWrap: true,
-                  primary: false,
-                  itemCount: products.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    Products pro = products[index];
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Divider(
+                color: Colors.pink.shade200,
+              ),
+              StreamBuilder<QuerySnapshot>(
+                  stream: productspre.snapshots(),
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    if (snapshot.hasError) {
+                      return Text('Something went wrong');
+                    }
 
-                    return Padding(
-                      padding: EdgeInsets.symmetric(vertical: 20.0),
-                      child: ListTile(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => BookPage(),
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                    var products = snapshot.data!.docs
+                        .map((doc) => Products.fromFirestore(doc))
+                        .toList();
+                    // final searchResults = products
+                    //     .where((product) => product.title!
+                    //         .toString()
+                    //         .toLowerCase()
+                    //         .contains(searchQuery.toLowerCase()))
+                    //     .toList();
+
+                    products = products
+                        .where((product) => product.title!
+                            .toString()
+                            .toLowerCase()
+                            .contains(searchQuery.toLowerCase()))
+                        .toList();
+                    return ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      primary: false,
+                      itemCount: products.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        Products pro = products[index];
+
+                        return Padding(
+                          padding: EdgeInsets.symmetric(vertical: 20.0),
+                          child: ListTile(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => BookPage(),
+                                ),
+                              );
+                            },
+                            leading: Image.asset(pro.image),
+                            title: Text(
+                              pro.title,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
                             ),
-                          );
-                        },
-                        leading: Image.asset(pro.image),
-                        title: Text(
-                          pro.title,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
+                            trailing: Text(pro.price.toString()),
                           ),
-                        ),
-                        trailing: Text(pro.price.toString()),
-                      ),
+                        );
+                      },
                     );
-                  },
-                )
-              ],
-            ),
-          );
-        },
-      ),
-    );
+                  })
+            ],
+          ),
+        ));
   }
 }
